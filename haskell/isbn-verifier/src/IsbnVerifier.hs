0ec4e78868627da6f16259aed6f265ffb35944ea
benjamin.hall <benjamin.hall@rea-group.com>
2018-01-12 13:21:13 +1100

    Update code.

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -8,7 +8,7 @@ isbn = validIsbn . convert . convertToNumberArray . keepValidCharacters
 validIsbn :: [Int] -> Bool
 validIsbn input
   | length input /= 10 = False
-  | sum input `mod` 11 == 0 = True
+  | sum input `rem` 11 == 0 = True
   | otherwise = False
 
 keepValidCharacters :: String -> String
@@ -22,5 +22,5 @@ convertCharToInt 'X' = 10
 convertCharToInt x = digitToInt x
 
 convert :: [Int] -> [Int]
-convert = zipWith (*) $ reverse [0..10]
+convert = zipWith (*) $ [10,9..]
 
