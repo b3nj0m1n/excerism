edaedcab18091871f778a03459ee663034b032dc
b3nj0m1n <captainhall@gmail.com>
2018-01-18 20:45:53 +1100

    Haskell encoding working :)

--- haskell/rail-fence-cipher/src/RailFenceCipher.hs
+++ haskell/rail-fence-cipher/src/RailFenceCipher.hs
@@ -1,7 +1,17 @@
-module RailFenceCipher (encode, decode) where
+module RailFenceCipher (encode, decode)
+where
+
+import Data.Ord
+import Data.List
 
 encode :: Int -> String -> String
-encode size decoded = decoded
+encode size decoded = map fst (toTuple decoded (rows size))
+
+toTuple :: String -> [Int] -> [(Char, Int)]
+toTuple s i = sortBy (comparing  snd) (zip s (cycle i))
+
+rows :: Int -> [Int]
+rows t = [1..t] ++ (reverse [2..t-1])
 
 decode :: Int -> String -> String
 decode a b = "You need to implement this function!"
