37ce892f2f4e17c4708c4caa0b9ce36c11706e46
b3nj0m1n <captainhall@gmail.com>
2018-01-18 22:16:34 +1100

    haskell fun

--- haskell/rail-fence-cipher/src/RailFenceCipher.hs
+++ haskell/rail-fence-cipher/src/RailFenceCipher.hs
@@ -5,13 +5,23 @@ import Data.Ord
 import Data.List
 
 encode :: Int -> String -> String
-encode size decoded = map fst (toTuple decoded (rows size))
+encode size decoded = map fst (toTupleWithSort decoded (rows size))
 
-toTuple :: String -> [Int] -> [(Char, Int)]
-toTuple s i = sortBy (comparing  snd) (zip s (cycle i))
+toTupleWithSort :: String -> [Int] -> [(Char, Int)]
+toTupleWithSort s i = sortBy (comparing  snd) (zip s (cycle i))
 
 rows :: Int -> [Int]
 rows t = [1..t] ++ (reverse [2..t-1])
 
 decode :: Int -> String -> String
-decode a b = "You need to implement this function!"
+decode size encoded = removeDot (t size encoded)
+
+t :: Int -> String -> String
+t size encoded = concat (map (: space  size) encoded)
+
+space :: Int -> String
+space i = concat (replicate i ".")
+
+
+removeDot :: String -> String
+removeDot xs = [ x | x <- xs, not (x `elem` ".") ]
