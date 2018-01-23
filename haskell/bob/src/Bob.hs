b8fc49e6d96f8ac1987c7bee91f09b8af5207abe
benjamin.hall <benjamin.hall@rea-group.com>
2018-01-23 17:09:31 +1100

    Haskell fun

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,7 +1,26 @@
 module Bob (responseFor) where
 
 responseFor :: String -> String
-responseFor "" = "Fine. Be that way!"
-responseFor "SHOUT" = "Whoa, chill out!"
-responseFor "SHOUT QUESTION" = "Calm down, I know what I'm doing!"
-responseFor xs = "Whatever."
+responseFor xs
+  | silent xs == ""  = "Fine. Be that way!"
+  | shoutQuestion xs = "Calm down, I know what I'm doing!"
+  | shout xs  = "Whoa, chill out!"
+  | question xs = "Sure."
+  | otherwise = "Whatever."
+
+silent :: String -> String
+silent [] = ""
+silent xs = onlyCharacters xs ++ [x | x <- xs, elem x (['0'..'9'] ++ [':','?'])]
+
+shout :: String -> Bool
+shout xs = (onlyCharacters xs) /= "" && length([x | x <- onlyCharacters xs, elem x (['A'..'Z'] )] ) == length (onlyCharacters xs)
+
+shoutQuestion :: String -> Bool
+shoutQuestion x = shout x && question x
+
+question :: String -> Bool
+question x = last x == '?'
+
+onlyCharacters :: String -> String
+onlyCharacters xs = [x | x <- xs, elem x  (['A'..'Z'] ++ ['a'..'z'])]
+
