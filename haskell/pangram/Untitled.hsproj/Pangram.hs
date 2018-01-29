42b482a39f4e664815ece68981ba7cefe8d94e75
benjamin.hall <benjamin.hall@rea-group.com>
2018-01-30 08:45:06 +1100

    Clean up

--- haskell/pangram/Untitled.hsproj/Pangram.hs
+++ haskell/pangram/Untitled.hsproj/Pangram.hs
@@ -3,8 +3,9 @@ module Pangram (isPangram) where
 import Data.Char
 
 isPangram :: String -> Bool
-isPangram xs = length (filter (>0) ve) == 26
-   where ve = map (numChar xs) ['a'..'z']
+isPangram xs = length (filter (>0) ve) == length alphabet
+   where ve = map (numChar xs) alphabet
+         alphabet = ['a'..'z']  
 
 numChar :: String -> Char -> Int
 numChar xs c = length $ filter (== c) lc
