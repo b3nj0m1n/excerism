731a06f14bd7a8bedbacf54306d46ecf9dbb61e4
benjamin.hall <benjamin.hall@rea-group.com>
2018-01-30 11:03:42 +1100

    Swift list ops

---
+++ swift/list-ops/Sources/ListOps.swift
@@ -0,0 +1,9 @@
+class ListOps {
+    class func append(_ i: [Int], _ a: [Int]) -> [Int] {
+        return [Int]()
+    }
+    
+    class func concat(_ i: [Int]) -> [Int] {
+        return [Int]()
+    }
+}
